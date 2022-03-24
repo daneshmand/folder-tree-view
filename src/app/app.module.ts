245072745392dc7096a6da323debef03bb456dbd
PC-Desktop\Ali <harandi@creativecorecloud.com>
2022-03-24 15:08:03 +0430

    change code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NodeComponent } from './node/node.component';
+import {AppComponent} from './app.component';
+import {NodeComponent} from './node/node.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { TransformToJsonPipe } from './transform-to-json.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NodeComponent,
-    TransformToJsonPipe,
-  ],
+    declarations: [
+        AppComponent,
+        NodeComponent
+    ],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         FormsModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/node/node.model.ts
+++ src/app/node/node.model.ts
@@ -1,38 +1,39 @@
 export class NodeModel {
-  type: 'folder' | 'file' | 'unset' | null;
-  name?: string;
-  children?: NodeModel[];
-  id: string;
+    type: 'folder' | 'file' | 'unset' | null;
+    name?: string;
+    children?: NodeModel[];
+    id: string;
 
-  constructor(type: 'folder' | 'file' | 'unset' | null, id: string, options?: {
-    name?: string
-  }) {
-    this.type = type;
-    this.id = id;
-    this.name = !!options && !!options.name ? options.name : 'Untitled';
-  }
+    constructor(type: 'folder' | 'file' | 'unset' | null, id: string, options?: {
+        name?: string
+    }) {
+        this.type = type;
+        this.id = id;
+        this.name = !!options && !!options.name ? options.name : 'Untitled';
+    }
 }
 
 export class Node extends NodeModel {
-  icon?: 'file' | 'folder' | 'unset';
-  children?: Node[];
-  isFocused: boolean;
-  isEditing: boolean;
-  index: number;
-  parentId: string | null;
+    icon?: 'file' | 'folder' | 'unset';
+    children?: Node[];
+    isFocused: boolean;
+    isEditing: boolean;
+    index: number;
+    parentId: string | null;
 
-  constructor(type: 'folder' | 'file' | 'unset' | null, id: string, index: number, parentId: string | null, options?: {
-    name?: string, children?: Node[], icon?: 'file' | 'folder' | 'unset'
-  }) {
-    super(type, id, options);
-    this.parentId = parentId;
-    this.icon = !!options && !!options.icon ? options.icon : 'unset';
-    this.isFocused = true;
-    this.isEditing = true;
-    this.index = index;
-    this.children = !!options && !!Array.isArray(options.children) ? options.children : [];
-  }
-  nodeHasChildren(): boolean {
-      return !!this.children && this.children.length > 0;
-  }
+    constructor(type: 'folder' | 'file' | 'unset' | null, id: string, index: number, parentId: string | null, options?: {
+        name?: string, children?: Node[], icon?: 'file' | 'folder' | 'unset'
+    }) {
+        super(type, id, options);
+        this.parentId = parentId;
+        this.icon = !!options && !!options.icon ? options.icon : 'unset';
+        this.isFocused = true;
+        this.isEditing = true;
+        this.index = index;
+        this.children = !!options && !!Array.isArray(options.children) ? options.children : [];
+    }
+
+    nodeHasChildren(): boolean {
+        return !!this.children && this.children.length > 0;
+    }
 }
